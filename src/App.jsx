517590ec09f7ef3fa477abceff24cc8037dab99b
Maty910 <matychacong@gmail.com>
2025-01-27 22:40:29 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+function App() {
+
+  return (
+    <>
+      <header>
+        <img src="" alt="" />
+        <h1>Lucía Castro</h1>
+        <h2>Social Media Manager</h2>
+        <nav>
+          <ul>
+            <li><button>Inicio</button></li>
+            <li><button>Proyectos</button></li>
+            <li><button>Experiencia</button></li>
+            <li><button>Contacto</button></li>
+          </ul>
+        </nav>
+        <section>
+          <h3>¡Hola! Soy Lucía Castro, Social Media Manager!</h3>
+          <p>Me dedico a la gestión de redes sociales desde hace 5 años. Me apasiona el mundo digital y me encanta ayudar a las empresas a crecer en el mundo online.</p>
+          <button>Contáctame en LinkedIn</button>
+          <img src="" alt="" />
+        </section>
+        <section>
+          <h3>Proyectos</h3>
+          <p>Estos son algunos de los proyectos en los que he trabajado:</p>
+          <ul>
+            <li>
+              <img src="" alt="" />
+              <button>Proyecto 1</button>
+            </li>
+            <li>
+              <img src="" alt="" />
+              <button>Proyecto 2</button>
+            </li>
+            <li>
+              <img src="" alt="" />
+              <button>Proyecto 3</button>
+            </li>
+          </ul>
+        </section>
+        <section>
+          <h3>Experiencia</h3>
+          <p>He trabajado en diferentes empresas como Social Media Manager:</p>
+          <ul>
+            <li>Empresa 1</li>
+            <li>Empresa 2</li>
+            <li>Empresa 3</li>
+          </ul>
+        </section>
+        <section>
+          <h3>Servicios</h3>
+          <p>Estos son algunos de los servicios que ofrezco:</p>
+        </section>
+        <section>
+          <h3>Contacto</h3>
+          <p>Si quieres contactar conmigo, puedes hacerlo a través de LinkedIn o enviándome un correo electrónico a:</p>
+          <a href="mailto"> </a>
+          <form action="">
+            <input type="text" placeholder="Nombre" />
+            <input type="email" placeholder="Correo electrónico" />
+            <textarea name="" id="" cols="30" rows="10" placeholder="Mensaje"></textarea>
+            <button>Enviar</button>
+          </form>
+        </section>
+        <footer>
+          <p>© 2025 Lucía Castro. Todos los derechos reservados.</p>
+        </footer>
+      </header>
+    </>
+  )
+}
+
+export default App
