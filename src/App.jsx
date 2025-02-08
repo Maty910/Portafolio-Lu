9da770c2b703de6a99e90e2db1a29e37d5755c34
Maty910 <matychacong@gmail.com>
2025-02-08 12:27:39 -0300

    feat: home background texture

--- src/App.jsx
+++ src/App.jsx
@@ -11,27 +11,29 @@ function App() {
   return (
     <>
       <div className="scroll-container">
-        <nav>
-          <ul>
-            <li><button><a href="#about">{language === 'es' ? 'Sobre mí' : 'About me'}</a></button></li>
-            <li><button><a href="#experience">{language === 'es' ? 'Experiencia' : 'Experience'}</a></button></li>
-            <li><button><a href="#contact">{language === 'es' ? 'Contactame' : 'Message me'}</a></button></li>
-            <button className="language" onClick={toggleLanguage}>
-              {language === 'es' ? 'English' : 'Español'}
-            </button>
-          </ul>
-        </nav>
-        <header className={`home ${language}`} id="home">
-          <div className="home-content">
-            <h1 data-text={language === 'es' ? 'El verdadero crecimiento está en aprender cada día' : 'True growth lies in learning every day'}>
-              {language === 'es' ? 'El verdadero crecimiento' : 'True growth'} <br/> 
-              {language === 'es' ? 'está en' : 'lies in'} <br/> 
-              {language === 'es' ? 'aprender' : 'learning'} <br/> 
-              {language === 'es' ? 'cada día' : 'every day'}
-            </h1>
-            <button className="seework"><a href="#about">{language === 'es' ? '¡Ver más!' : 'Learn more!'}</a></button>
-          </div>
-        </header>
+        <div className='home-background'>
+          <nav>
+            <ul>
+              <li><button><a href="#about">{language === 'es' ? 'Sobre mí' : 'About me'}</a></button></li>
+              <li><button><a href="#experience">{language === 'es' ? 'Experiencia' : 'Experience'}</a></button></li>
+              <li><button><a href="#contact">{language === 'es' ? 'Contactame' : 'Message me'}</a></button></li>
+              <button className="language" onClick={toggleLanguage}>
+                {language === 'es' ? 'English' : 'Español'}
+              </button>
+            </ul>
+          </nav>
+          <header className={`home ${language}`} id="home">
+            <div className="home-content">
+              <h1 data-text={language === 'es' ? 'El verdadero crecimiento está en aprender cada día' : 'True growth lies in learning every day'}>
+                {language === 'es' ? 'El verdadero crecimiento' : 'True growth'} <br/> 
+                {language === 'es' ? 'está en' : 'lies in'} <br/> 
+                {language === 'es' ? 'aprender' : 'learning'} <br/> 
+                {language === 'es' ? 'cada día' : 'every day'}
+              </h1>
+              <button className="seework"><a href="#about">{language === 'es' ? '¡Ver más!' : 'Learn more!'}</a></button>
+            </div>
+          </header>
+        </div>
         <section className="about" id="about">
           <div>
             <img src="/img/PERFIL.svg" />
